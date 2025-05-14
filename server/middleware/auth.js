b35782ec99cd3a1942d789c9ec3125f136173295
Lars <144362461+laha0006@users.noreply.github.com>
2025-05-14 21:07:14 +0200

    changed status code to 401 if no token

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,20 +3,20 @@ import jwt from "jsonwebtoken";
 export function authenticateToken(req, res, next) {
     const cookies = req.cookies;
     if (!cookies.accessToken) {
-        return res.status(403).send({ message: "no access token" });
+        return res.status(401).send({ message: "no access token" });
     }
 
     jwt.verify(
         cookies.accessToken,
         process.env.ACCESS_TOKEN_SECRET,
-        (err, token) => {
+        (err, decoded) => {
             if (err) {
                 console.log(err);
                 return res.sendStatus(403);
             }
             req.user = {
-                username: token.username,
-                email: token.email,
+                username: decoded.username,
+                email: decoded.email,
             };
             next();
         }
