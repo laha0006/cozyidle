80829bd334f3e66f39304bdab82d5f02805bffc1
Lars <144362461+laha0006@users.noreply.github.com>
2025-05-14 18:19:36 +0200

    implemented basic express and io server

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { Server} from "socket.io";
+import { Server } from "socket.io";
 import http from "http";
-
-const test = [1,2,3]
 
 const app = express();
 const server = http.createServer(app);
-const io = 
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    console.log("A socket connected", socket.id);
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, console.log(`Server listening on ${PORT}`));
