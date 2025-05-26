e7b3cde61aaf948291a3534401bdf5ae2006a07a
Lars <144362461+laha0006@users.noreply.github.com>
2025-05-26 14:09:44 +0200

    implemented fetch.js utility that will automatically try to refresh token

--- client/src/util/fetch.js
+++ client/src/util/fetch.js
@@ -1,9 +1,65 @@
 //TODO: implement fetch methods
+import { user } from "../stores/userStore.js";
 
-export async function postFetch(url) {
-    console.log("post fetch yay!");
+export async function postFetch(url, data) {
+    let response = await fetch(url, {
+        credentials: "include",
+        method: "POST",
+        headers: {
+            "Content-Tupe": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+        if (await refreshFetch()) {
+            response = await postFetch(url, data);
+        } else {
+            const json = await response.json();
+            console.log(json.message);
+            return false;
+        }
+    }
+    const json = await response.json();
+    console.log(json.message);
+    return json;
 }
 
 export async function getFetch(url) {
-    console.log("get fetch yay!");
+    let response = await fetch(url, {
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    if (!response.ok) {
+        if (await refreshFetch()) {
+            response = await getFetch(url);
+        } else {
+            const json = await response.json();
+            console.log(json.message);
+            return false;
+        }
+    }
+    const json = await response.json();
+    console.log(json.message);
+    return json;
+}
+
+export async function refreshFetch() {
+    console.log("refreshFetch!");
+    const response = await fetch("/api/refresh", {
+        credentials: "include",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    if (!response.ok) {
+        user.set(null);
+        return false;
+    }
+    const json = await response.json();
+    user.set(json.user);
+    return true;
 }
