8763c0bfbb11787242b9449be879673a0b8897b9
Lars <144362461+laha0006@users.noreply.github.com>
2025-05-26 15:44:27 +0200

    signup now returns a boolean depending on if signup was succesful or not

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -4,9 +4,8 @@ import { postFetch } from "../util/fetch";
 export async function signup(formData) {
     const { response, json } = await postFetch("/api/signup", formData);
     if (!response.ok) {
-        //TODO: toast
-        return;
+        return false;
     }
-    console.log("succesfully signed up!");
     user.set(json.user);
+    return true;
 }
