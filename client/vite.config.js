b8912d8e39425b0dcc63edad18100dd518611710
Lars <144362461+laha0006@users.noreply.github.com>
2025-05-26 13:31:53 +0200

    added proxy for /api endpoints

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [svelte(), tailwindcss()],
+    server: {
+        proxy: {
+            // Proxy all requests starting with /api to your Express server
+            "/api": {
+                target: "http://localhost:8080",
+                changeOrigin: true,
+            },
+        },
+    },
 });
