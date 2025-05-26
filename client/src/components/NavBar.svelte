15a4b708ee22fb7929c2e123a7f7b10e5aa46ae2
Lars <144362461+laha0006@users.noreply.github.com>
2025-05-26 14:09:09 +0200

    added new nav links to NavBar

--- client/src/components/NavBar.svelte
+++ client/src/components/NavBar.svelte
@@ -9,7 +9,17 @@
     function goToTest() {
         router.navigate("/test/10/420");
     }
+
+    function goToLogin() {
+        router.navigate("/login");
+    }
+
+    function goToSignup() {
+        router.navigate("/signup");
+    }
 </script>
 
 <button onclick={goToHome}> Home</button>
 <button onclick={goToTest}> Test</button>
+<button onclick={goToLogin}> Login</button>
+<button onclick={goToSignup}> Signup</button>
