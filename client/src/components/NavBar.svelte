0427ecabf5806116cd94b437b4c0336d3a699fbd
Lars <144362461+laha0006@users.noreply.github.com>
2025-05-26 21:38:16 +0200

    added user.id to navbar instead of username

--- client/src/components/NavBar.svelte
+++ client/src/components/NavBar.svelte
@@ -1,6 +1,9 @@
 <script>
     import { useTinyRouter } from "svelte-tiny-router";
     import { user } from "../stores/userStore.js";
+    import { logout } from "../api/auth.js";
+    import { toast } from "@zerodevx/svelte-toast";
+    import { success } from "../util/toasts.js";
     const router = useTinyRouter();
 
     function goToHome() {
@@ -18,6 +21,16 @@
     function goToSignup() {
         router.navigate("/signup");
     }
+
+    async function handleLogout() {
+        try {
+            const json = await logout();
+            user.set(null);
+            success(json.message);
+        } catch (error) {
+            toast.push(error.message);
+        }
+    }
 </script>
 
 <button onclick={goToHome}> Home</button>
@@ -25,4 +38,7 @@
 {#if !$user}
     <button onclick={goToLogin}> Login</button>
     <button onclick={goToSignup}> Signup</button>
+{:else}
+    <button onclick={handleLogout}> Log out</button>
+    {$user.id}
 {/if}
