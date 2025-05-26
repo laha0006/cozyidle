3ba7b311906ae8a9f1ecfcacbb84986ad6d39beb
Lars <144362461+laha0006@users.noreply.github.com>
2025-05-26 21:09:25 +0200

    extracted guard logic to guards.js

---
+++ client/src/util/guards.js
@@ -0,0 +1,14 @@
+import { user } from "../stores/userStore.js";
+
+function isAutenticated() {
+    return Boolean($user);
+}
+
+export const authGuard = async ({ to, from, next }) => {
+    if (to.path.startsWith("/game") && !isAuthenticated()) {
+        //TODO: base64 encode from, is probably a good idea
+        next({ path: `/?from=${to.path}`, replace: true });
+    } else {
+        next();
+    }
+};
