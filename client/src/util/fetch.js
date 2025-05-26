7c735cfb721ab82001ae708f58787e9868dc6eae
Lars <144362461+laha0006@users.noreply.github.com>
2025-05-26 13:37:39 +0200

    added fetch.js utility for api calls

---
+++ client/src/util/fetch.js
@@ -0,0 +1,9 @@
+//TODO: implement fetch methods
+
+export async function postFetch(url) {
+    console.log("post fetch yay!");
+}
+
+export async function getFetch(url) {
+    console.log("get fetch yay!");
+}
