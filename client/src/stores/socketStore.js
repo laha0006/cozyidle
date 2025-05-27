d3652badd2ac58374795f7692421d3f9fd9e9bd9
Lars <144362461+laha0006@users.noreply.github.com>
2025-05-27 21:35:32 +0200

    started initial implementation of scoketStore

--- client/src/stores/socketStore.js
+++ client/src/stores/socketStore.js
@@ -1,1 +1,13 @@
 import { readable } from "svelte/store";
+
+const socket = io("http://localhost:8080");
+
+socket.on("connect", () => {
+    console.log("socket connected!");
+});
+
+socket.on("connect_error", (error) => {
+    console.log("error");
+});
+
+export const socketStore = readable(sokcet);
