280cb98a6d839a021d2d404a279482b1b7546e5b
Lars <144362461+laha0006@users.noreply.github.com>
2025-05-27 23:35:28 +0200

    changed socket store to be derived from userStore

--- client/src/stores/socketStore.js
+++ client/src/stores/socketStore.js
@@ -1,32 +1,60 @@
 import { io } from "socket.io-client";
-import { writable } from "svelte/store";
+import { writable, derived, get } from "svelte/store";
+import { user } from "./userStore.js";
 
-export const socketStore = writable(null);
+// export const socketStore = writable(null);
 
-let socket = null;
+// let socket = null;
 
-export function initSocket() {
-    if (socket) {
-        socket.disconnect();
+// export function initSocket() {
+//     if (socket) {
+//         socket.disconnect();
+//     }
+
+//     console.log("Initializing socket connection...");
+//     socket = io("http://localhost:8080", { withCredentials: true });
+
+//     socket.on("connect", () => {
+//         console.log("socket connected!");
+//     });
+
+//     socket.on("connect_error", (error) => {
+//         console.log("error", error);
+//     });
+//     socketStore.set(socket);
+// }
+
+// export function disconnectSocket() {
+//     if (socket) {
+//         socket.disconnect();
+//         socket = null;
+//     }
+//     socketStore.set(null);
+// }
+
+export const socketStore = derived(user, ($user, set) => {
+    let socket;
+    if ($user) {
+        console.log("DERIVED YO");
+
+        socket = io("http://localhost:8080", { withCredentials: true });
+        set(socket);
+
+        socket.on("connect", () => {
+            console.log("Socket connected!");
+        });
+        socket.on("connect_error", (error) => {
+            console.log("Socket error:", error);
+        });
+    } else {
+        console.log("else");
+        set(null);
     }
-
-    console.log("Initializing socket connection...");
-    socket = io("http://localhost:8080", { withCredentials: true });
-
-    socket.on("connect", () => {
-        console.log("socket connected!");
-    });
-
-    socket.on("connect_error", (error) => {
-        console.log("error", error);
-    });
-    socketStore.set(socket);
-}
-
-export function disconnectSocket() {
-    if (socket) {
-        socket.disconnect();
-        socket = null;
-    }
-    socketStore.set(null);
-}
+    return () => {
+        console.log("cleanup");
+        if (socket) {
+            socket.disconnect();
+        }
+        set(null);
+    };
+});
