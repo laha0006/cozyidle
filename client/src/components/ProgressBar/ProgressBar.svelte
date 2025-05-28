442015b132b6b0026818e0f76fb9021d7114b9b0
Lars <144362461+laha0006@users.noreply.github.com>
2025-05-28 23:55:35 +0200

    lifted some state, and removed onTick

--- client/src/components/ProgressBar/ProgressBar.svelte
+++ client/src/components/ProgressBar/ProgressBar.svelte
@@ -1,28 +1,39 @@
 <script>
     import { tweened } from "svelte/motion";
-    import { linear } from "svelte/easing";
+    import { expoOut, linear } from "svelte/easing";
     import { onMount } from "svelte";
 
-    const { duration, repeat } = $props();
-
-    let count = 0;
-
+    let { duration, repeat } = $props();
+    console.log("init duration: ", duration);
     const progress = tweened(0, { duration: duration, easing: linear });
 
     function reset() {
-        count++;
         progress.set(0, { duration: 0 });
-        progress.set(1);
+        progress.set(1, { duration: duration });
     }
 
     function start() {
-        progress.set(1);
+        progress.set(1, { duration: duration });
     }
 
+    $effect(() => {
+        if (repeat) {
+            console.log("duration:", duration);
+            console.log("repeat", repeat);
+            console.log("effect start");
+            start();
+        } else {
+            progress.set(0, { duration: 0 });
+        }
+    });
+
     onMount(() => {
-        start();
         const unsubscribe = progress.subscribe((value) => {
+            if (value === 1) {
+                // onTick();
+            }
             if (value === 1 && repeat) {
+                console.log("reset");
                 reset();
             }
             if (value === 1 && !repeat) {
