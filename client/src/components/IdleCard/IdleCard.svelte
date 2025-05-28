b3250c25d1fba74fe174d265bb847485cd7a0c0d
Lars <144362461+laha0006@users.noreply.github.com>
2025-05-28 22:43:42 +0200

    implementing IdleCard

--- client/src/components/IdleCard/IdleCard.svelte
+++ client/src/components/IdleCard/IdleCard.svelte
@@ -1,7 +1,21 @@
 <script>
     import ProgressBar from "../ProgressBar/ProgressBar.svelte";
+    let count = $state(0);
+    let duration = $state(0);
+    let repeat = $state(true);
+
+    function start() {
+        console.log("start");
+        duration = 2000;
+    }
 </script>
 
 <div>
-    <ProgressBar />
+    <h1>IdleCard</h1>
+    <div>
+        {count}
+    </div>
+    <ProgressBar {duration} {repeat} />
+    <button onclick={start}>Start</button>
+    <button>Stop</button>
 </div>
