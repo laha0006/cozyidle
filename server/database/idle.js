4b56479fb70b0aad5abed43ca00f3abfb9404c36
Lars <144362461+laha0006@users.noreply.github.com>
2025-05-30 00:11:35 +0200

    implemented updateIdle database function

--- server/database/idle.js
+++ server/database/idle.js
@@ -17,3 +17,21 @@ export async function getIdle(userId) {
     const res = await db.query(sql, values);
     return res.rows[0].started_unix * 1000;
 }
+
+export async function updateIdle(userId) {
+    const sql = `WITH old AS (
+                SELECT started FROM user_idles WHERE user_id = $1 FOR UPDATE
+                ),
+                updated AS (
+                UPDATE user_idles 
+                SET started = NOW()
+                WHERE user_id = $1
+                )
+                UPDATE user_resources
+                SET count = count + FLOOR(EXTRACT(EPOCH FROM (NOW() - (SELECT started FROM old))) / 2)
+                WHERE user_id = $1;`;
+    const values = [userId];
+
+    const res = await db.query(sql, values);
+    return res;
+}
