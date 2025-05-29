3dd6b838d09dda0c3ccbec728d31c3ea84033b08
Lars <144362461+laha0006@users.noreply.github.com>
2025-05-29 23:45:44 +0200

    implemented getIdle function

--- server/database/idle.js
+++ server/database/idle.js
@@ -1,8 +1,17 @@
 import db from "./connection.js";
 
-async function startIdle(userId) {
+export async function startIdle(userId) {
     const sql =
         "UPDATE user_idles SET started = NOW(), active = TRUE WHERE user_id = $1";
+    const values = [userId];
+
+    const res = await db.query(sql, values);
+    return res;
+}
+
+export async function getIdle(userId) {
+    const sql =
+        "SELECT EXTRACT(EPOCH FROM started) AS started_unix FROM user_idles WHERE user_id = $1";
     const values = [userId];
 
     const res = await db.query(sql, values);
