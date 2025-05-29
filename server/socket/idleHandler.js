bdae2ac33b33f9336719c4a36e9e1aa021b09ede
Lars <144362461+laha0006@users.noreply.github.com>
2025-05-29 23:05:42 +0200

    started implementing idle handler for socket

---
+++ server/socket/idleHandler.js
@@ -0,0 +1,16 @@
+export function idleDispatch(event, data) {
+    switch (event) {
+        case "start":
+            {
+            }
+            break;
+        case "stop":
+            {
+            }
+            break;
+        case "sync":
+            {
+            }
+            break;
+    }
+}
