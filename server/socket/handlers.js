2732c9ea6ec9039f3c564d4d540cd2dd997f9af3
Lars <144362461+laha0006@users.noreply.github.com>
2025-05-31 00:15:19 +0200

    implemented registerIdleHandlers

--- server/socket/handlers.js
+++ server/socket/handlers.js
@@ -1,1 +1,12 @@
+import { IdleClientEvent } from "./events/idleEvents.js";
 import { idleDispatch } from "./idleHandler.js";
+
+export function registerIdleHandlers(socket) {
+    const events = Object.values(IdleClientEvent);
+
+    events.forEach((event) => {
+        socket.on(event, (...args) => {
+            idleDispatch(event, socket, ...args);
+        });
+    });
+}
