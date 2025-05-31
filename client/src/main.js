a237656d830a61644afff8274adf2ac788cba780
Lars <144362461+laha0006@users.noreply.github.com>
2025-05-31 02:20:38 +0200

    removed debug logs

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,6 @@ import { mount } from "svelte";
 import "./app.css";
 import App from "./App.svelte";
 
-console.log("main.js loaded");
-
 const app = mount(App, {
     target: document.getElementById("app"),
 });
