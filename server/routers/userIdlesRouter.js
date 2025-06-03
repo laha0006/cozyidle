38bba6bb1b11668a561e37ccc9cea1521a82650e
Lars <144362461+laha0006@users.noreply.github.com>
2025-06-04 01:25:56 +0200

    implemented userIdlesRouter

--- server/routers/userIdlesRouter.js
+++ server/routers/userIdlesRouter.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
+import db from "../database/connection.js";
 
 const router = Router();
 
-router.get("/users/:userId/idles", async (req, res) => {
-    console.log("get idle for user!");
+router.get("/api/users/:userId/idles", async (req, res) => {
+    console.log("/api/users/id/idles hit!");
+    const { userId } = req.params;
+    const sql = `
+        SELECT * FROM user_idles
+        WHERE user_id = $1
+    `;
+
+    const { rows } = await db.query(sql, [userId]);
+    res.send({ data: rows });
 });
 
 export default router;
