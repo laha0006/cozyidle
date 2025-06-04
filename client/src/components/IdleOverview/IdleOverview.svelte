4f80849538d2d3c8d6ca0b81f088b8bb7074a4fd
Lars <144362461+laha0006@users.noreply.github.com>
2025-06-04 02:23:47 +0200

    started on IdleOverview implementation

--- client/src/components/IdleOverview/IdleOverview.svelte
+++ client/src/components/IdleOverview/IdleOverview.svelte
@@ -1,4 +1,16 @@
 <script>
+    import { onMount } from "svelte";
+    import { getFetchWithRefresh } from "../../util/fetch";
+    import { user } from "../../stores/userStore.js";
+
+    $effect(async () => {
+        if ($user) {
+            const init = await getFetchWithRefresh(
+                "/api/users/" + $user.id + "/idles"
+            );
+            console.log("init: ", init);
+        }
+    });
 </script>
 
 <h1>IdleOverview</h1>
