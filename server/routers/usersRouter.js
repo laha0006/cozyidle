9c682c3b6e14d2bcb35a4b9a640947819b2b3bae
Lars <144362461+laha0006@users.noreply.github.com>
2025-06-04 02:24:31 +0200

    activated middleware again

--- server/routers/usersRouter.js
+++ server/routers/usersRouter.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { authenticateToken } from "../middleware/auth.js";
 const router = Router();
 
-// router.use(authenticateToken);
+router.use(authenticateToken);
 
 router.get("/", (req, res) => {
     res.send({ message: "Auth succesful!", user: req.user });
