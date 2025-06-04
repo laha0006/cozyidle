20536fef0e551ae5b92ca465c1981e72ad0b8852
Lars <144362461+laha0006@users.noreply.github.com>
2025-06-04 22:38:49 +0200

    implemnting idleStore

--- client/src/stores/idleStore.js
+++ client/src/stores/idleStore.js
@@ -1,6 +1,7 @@
 import { derived, get, writable } from "svelte/store";
 import { user } from "./userStore.js";
 import { getFetchWithRefresh } from "../util/fetch.js";
+import { socketStore } from "./socketStore.js";
 
 console.log("idleStore script");
 
@@ -12,8 +13,11 @@ function createIdleStore() {
     function loop() {
         update((idles) => {
             return idles.map((idle) => {
+                if (!idle.active) return idle;
+
                 const now = Date.now();
                 const progress = Math.min((now - idle.lastIncrement) / 2000, 1);
+                // console.log("progresS:", progress);
                 const incrementCount = Math.floor(
                     (now - idle.lastIncrement) / 2000
                 );
@@ -33,14 +37,12 @@ function createIdleStore() {
             const init = await getFetchWithRefresh(
                 "/api/users/" + $user.id + "/idles"
             );
-            const actives = init.data
-                .filter((idle) => idle.active)
-                .map((idle) => ({
-                    ...idle,
-                    lastIncrement: Date.now(),
-                    progress: 0,
-                }));
-            set(actives);
+            const idles = init.data.map((idle) => ({
+                ...idle,
+                lastIncrement: +idle.started,
+                progress: 0,
+            }));
+            set(idles);
             loop();
         } else {
             if (rafLoopId) {
@@ -54,6 +56,7 @@ function createIdleStore() {
     return {
         subscribe,
         set,
+        start: async (idleId) => {},
     };
 }
 
