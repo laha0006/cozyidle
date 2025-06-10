1757be1cf428d41311e28a150d8bada14962a04e
Lars <144362461+laha0006@users.noreply.github.com>
2025-06-10 21:17:13 +0200

    implemented item events

---
+++ server/socket/events/itemEvents.js
@@ -0,0 +1,11 @@
+export const ItemClientEvent = Object.freeze({
+    EQUIP: "item:client:equip",
+    UNEQIUP: "item:client:unequip",
+    BUY: "item:client:buy",
+});
+
+export const ItemServerEvent = Object.freeze({
+    EQUIPPED: "item:server:equipped",
+    UNEQUIPPED: "item:server:unequipped",
+    BOUGHT: "item:server:bought",
+});
