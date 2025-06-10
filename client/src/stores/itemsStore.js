093521eeae5dddb65c179bb2e3406cae2fb98944
Lars <144362461+laha0006@users.noreply.github.com>
2025-06-11 01:57:38 +0200

    removed commented out code

--- client/src/stores/itemsStore.js
+++ client/src/stores/itemsStore.js
@@ -6,21 +6,3 @@ export const itemsStore = writable(null);
 
 const items = await getFetchWithRefresh("/api/items");
 itemsStore.set(items.data);
-
-console.log("all items:", items.data);
-
-// export const unownedItems = derived(
-//     [itemsStore, userItemStore],
-//     ([$itemsStore, $userItemStore], set) => {
-//         if ($itemsStore && $userItemStore) {
-//             console.log("both!");
-//             const unownedItems = $itemsStore.filter((item) => {
-//                 return !$userItemStore.some(
-//                     (userItem) => userItem.item_id === item.item_id
-//                 );
-//             });
-//             set(unownedItems);
-//             console.log("unowned:", unownedItems);
-//         }
-//     }
-// );
