363369b4f64b0239d0dc723461658fba8282fd57
Lars <144362461+laha0006@users.noreply.github.com>
2025-06-10 22:41:44 +0200

    added itemDispatch to handler.js

--- server/socket/handlers.js
+++ server/socket/handlers.js
@@ -1,27 +1,32 @@
 import { IdleClientEvent } from "./events/idleEvents.js";
+import { ItemClientEvent } from "./events/itemEvents.js";
 import { idleDispatch } from "./idleHandler.js";
+import { itemDispatch } from "./itemHandler.js";
 
 const userLocks = new Map();
 
 export function registerIdleHandlers(socket) {
-    const events = Object.values(IdleClientEvent);
+    const idleEvents = Object.values(IdleClientEvent);
+    const itemEvents = Object.values(ItemClientEvent);
     const userId = socket.userId;
 
     if (!userLocks.get(userId)) {
-        // console.log("set false");
         userLocks.set(userId, false);
     }
-    events.forEach((event) => {
+    idleEvents.forEach((event) => {
         socket.on(event, async (...args) => {
             if (userLocks.get(userId)) {
-                console.log(">>>>>>>>>>>>>> DROPPED <<<<<<<<<<<<<<<");
                 return;
             }
             userLocks.set(userId, true);
-            console.log(event, socket.id, socket.userId);
             await idleDispatch(event, socket, ...args); // });
-            console.log(event, socket.id, socket.userId);
             userLocks.set(userId, false);
+        });
+    });
+
+    itemEvents.forEach((event) => {
+        socket.on(event, async (...args) => {
+            await itemDispatch(event, socket, ...args);
         });
     });
 }
