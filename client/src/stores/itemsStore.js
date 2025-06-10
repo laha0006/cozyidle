361c65652da93839db526ece02e64394be65069d
Lars <144362461+laha0006@users.noreply.github.com>
2025-06-11 01:48:42 +0200

    removed unowned items derived store

--- client/src/stores/itemsStore.js
+++ client/src/stores/itemsStore.js
@@ -9,18 +9,18 @@ itemsStore.set(items.data);
 
 console.log("all items:", items.data);
 
-export const unownedItems = derived(
-    [itemsStore, userItemStore],
-    ([$itemsStore, $userItemStore], set) => {
-        if ($itemsStore && $userItemStore) {
-            console.log("both!");
-            const unownedItems = $itemsStore.filter((item) => {
-                return !$userItemStore.some(
-                    (userItem) => userItem.item_id === item.item_id
-                );
-            });
-            set(unownedItems);
-            console.log("unowned:", unownedItems);
-        }
-    }
-);
+// export const unownedItems = derived(
+//     [itemsStore, userItemStore],
+//     ([$itemsStore, $userItemStore], set) => {
+//         if ($itemsStore && $userItemStore) {
+//             console.log("both!");
+//             const unownedItems = $itemsStore.filter((item) => {
+//                 return !$userItemStore.some(
+//                     (userItem) => userItem.item_id === item.item_id
+//                 );
+//             });
+//             set(unownedItems);
+//             console.log("unowned:", unownedItems);
+//         }
+//     }
+// );
