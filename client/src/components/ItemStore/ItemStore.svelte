b0bf7c7d7d767bb2136dcd59fb0f4e013c332b09
Lars <144362461+laha0006@users.noreply.github.com>
2025-06-11 00:22:34 +0200

    added ItemStore.svelte component

---
+++ client/src/components/ItemStore/ItemStore.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { unownedItems } from "../../stores/itemsStore.js";
+</script>
+
+{#each $unownedItems as item}
+    <div>
+        {item.name}
+        {item.price}
+    </div>
+    <div>
+        <button> buy </button>
+    </div>
+{/each}
