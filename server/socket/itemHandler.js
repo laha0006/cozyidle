f5fea887a44c8d7d6d04ce2538077c50b3f7ba9c
Lars <144362461+laha0006@users.noreply.github.com>
2025-06-10 22:07:34 +0200

    implemented equip and unequip event handling

--- server/socket/itemHandler.js
+++ server/socket/itemHandler.js
@@ -1,3 +1,4 @@
+import { equipItem, unequipItem } from "../database/idle";
 import { ItemClientEvent, ItemServerEvent } from "./events/itemEvents";
 
 export async function itemDispatch(event, socket, data) {
@@ -5,10 +6,24 @@ export async function itemDispatch(event, socket, data) {
     switch (event) {
         case ItemClientEvent.EQUIP:
             {
+                const { itemId } = data;
+                try {
+                    await equipItem(userId, itemId);
+                    socket.emit(ItemServerEvent.EQUIPPED);
+                } catch (error) {
+                    console.log(error);
+                }
             }
             break;
         case ItemClientEvent.UNEQIUP:
             {
+                const { itemId } = data;
+                try {
+                    await unequipItem(userId, itemId);
+                    socket.emit(ItemServerEvent.UNEQUIPPED);
+                } catch (error) {
+                    console.log(error);
+                }
             }
             break;
         case ItemClientEvent.BUY:
