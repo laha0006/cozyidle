f4cfe119780fbad3a8c7441b6f2c7bc71fb6f1e5
Lars <144362461+laha0006@users.noreply.github.com>
2025-06-10 17:35:20 +0200

    added items router

---
+++ server/routers/itemsRouter.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import db from "../database/connection.js";
+
+const router = Router();
+
+router.get("/items", async (req, res) => {
+    console.log("items endpoint");
+    const sql = `
+        SELECT 
+            i.id AS item_id,
+            i.name AS name,
+            s.name AS skill,
+            s.id AS skill_id,
+            i.bonus AS bonus,
+            i.price AS price,
+            i.skill_requirement AS requirement
+        FROM items i
+        JOIN skills s ON s.id = i.skill_id ;    
+    `;
+    const { rows } = await db.query(sql);
+    res.send({ data: rows });
+});
+
+export default router;
