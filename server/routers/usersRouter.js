11c5a9a4d7cc812272212730c31fa6e7d3034ad0
Lars <144362461+laha0006@users.noreply.github.com>
2025-06-11 19:07:47 +0200

    added skills to user routers

--- server/routers/usersRouter.js
+++ server/routers/usersRouter.js
@@ -25,7 +25,7 @@ router.get("/:userId/items", async (req, res) => {
     FROM items i
     JOIN skills s ON s.id = i.skill_id
     LEFT JOIN user_items ui
-        ON ui.item_id = i.id AND ui.user_id = $1`;
+    ON ui.item_id = i.id AND ui.user_id = $1`;
     const sql = `
     SELECT 
         i.name AS name,
@@ -44,6 +44,31 @@ router.get("/:userId/items", async (req, res) => {
 
     const { rows } = await db.query(allItemsSql, [userId]);
     res.send({ data: rows });
+});
+
+router.get("/:userId/skills", async (req, res) => {
+    const { userId } = req.params;
+    const sql = `
+    SELECT DISTINCT ON (sl.skill_id)
+        sl.level AS level,
+        sl.skill_id AS id,
+        s.name AS  name,
+        ue.experience AS experience
+    FROM user_experiences ue
+    JOIN skill_levels sl
+        ON sl.skill_id = ue.skill_id
+    JOIN skills s ON s.id = ue.skill_id
+    WHERE ue.user_id = $1
+    AND sl.experience_required <= ue.experience
+    ORDER BY sl.skill_id ,sl.level DESC
+    `;
+
+    try {
+        const res = await db.query(sql, [userId]);
+        res.send({ data: res.rows });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 export default router;
