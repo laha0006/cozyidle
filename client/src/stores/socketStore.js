29dcda9ce00aae9f2a8207ea0f0177727f071ce7
Lars <144362461+laha0006@users.noreply.github.com>
2025-06-11 17:35:47 +0200

    added error listener

--- client/src/stores/socketStore.js
+++ client/src/stores/socketStore.js
@@ -1,17 +1,23 @@
 import { io } from "socket.io-client";
 import { writable, derived, get } from "svelte/store";
 import { refreshUser, setUserIfAuthenticated, user } from "./userStore.js";
+import { toast } from "@zerodevx/svelte-toast";
+import { error } from "../util/toasts.js";
 
 let socket;
 export const socketStore = derived(user, async ($user, set) => {
     if ($user) {
-        // console.log("socketSTore init");
         if (socket) {
             socket.disconnect();
         }
 
         socket = io("http://localhost:8080", { withCredentials: true });
         set(socket);
+
+        socket.on("error", (data) => {
+            console.log("socket error:", data);
+            error(data.message);
+        });
 
         socket.on("connect", () => {
             // console.log("Socket connected!");
