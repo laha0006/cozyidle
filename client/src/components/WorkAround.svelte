9337bf5941a65bc936c761b08c78fc6e3a71ae14
Lars <144362461+laha0006@users.noreply.github.com>
2025-06-12 03:13:06 +0200

    added allStoresReady check

--- client/src/components/WorkAround.svelte
+++ client/src/components/WorkAround.svelte
@@ -1,20 +1,18 @@
 <script>
     import { onMount } from "svelte";
     import { setUserIfAuthenticated } from "../stores/userStore.js";
-    import { idleStore } from "../stores/idleStore.js";
     import { socketStore } from "../stores/socketStore.js";
-    import { userItemStore } from "../stores/userItemStore.js";
     import { itemsStore } from "../stores/itemsStore.js";
-    import { userSkillsStore } from "../stores/userSkillsStore.js";
-    import { userResourcesStore } from "../stores/userResourcesStore.js";
+    import { allUserStoresReady } from "../stores/init.js";
+    let init = false;
+    allUserStoresReady.subscribe(($ready) => {
+        if ($ready && !init) {
+            console.log(">>>>> All stores are ready!");
+            init = true;
+        }
+    });
 
     onMount(() => {
         setUserIfAuthenticated();
-        $socketStore,
-            $idleStore,
-            $userItemStore,
-            $itemsStore,
-            $userSkillsStore,
-            $userResourcesStore;
     });
 </script>
