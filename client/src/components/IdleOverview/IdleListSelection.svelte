1727d23634c040bbded296ee6920f248dace8d35
Lars <144362461+laha0006@users.noreply.github.com>
2025-06-15 07:08:08 +0200

    started on functionality

---
+++ client/src/components/IdleOverview/IdleListSelection.svelte
@@ -0,0 +1,28 @@
+<script>
+    import IdleCard from "../IdleCard/IdleCard.svelte";
+
+    const { idles } = $props();
+    let selected = $state(0);
+    let viewList = $state(false);
+
+    function select(index) {
+        selected = index;
+        toggleList();
+    }
+
+    function toggleList() {
+        console.log("view list clicked!");
+        viewList = !viewList;
+    }
+</script>
+
+{#if viewList}
+    {#each idles as idle, index}
+        <button onclick={() => select(index)}> {idle.idle}</button>
+    {/each}
+{:else}
+    <IdleCard idle={idles[selected]} />
+    <div>
+        <button onclick={toggleList}>Select</button>
+    </div>
+{/if}
--- client/src/components/IdleOverview/IdleOverview.svelte
+++ client/src/components/IdleOverview/IdleOverview.svelte
@@ -4,22 +4,18 @@
     import { user } from "../../stores/userStore.js";
     import IdleCard from "../IdleCard/IdleCard.svelte";
     import { idleStore, idleBySkillStore } from "../../stores/idleStore.js";
+    import IdleListSelection from "./IdleListSelection.svelte";
 </script>
 
 <h1>IdleOverview</h1>
-<!-- {#if $idleBySkillStore}
+{#if $idleBySkillStore}
     {#each $idleBySkillStore as idles}
-        <div>
-            {idles[0].skill}
-        </div>
-        <div>
-            <IdleCard idle={idles[0]} />
-        </div>
+        <IdleListSelection {idles} />
     {/each}
-{/if} -->
+{/if}
 
-{#if $idleStore}
+<!-- {#if $idleStore}
     {#each $idleStore as idle}
         <IdleCard {idle} />
     {/each}
-{/if}
+{/if} -->
