6f50665836b0bb1ca9478dd3bd545f7b1c38fa41
Lars <144362461+laha0006@users.noreply.github.com>
2025-06-16 03:48:32 +0200

    added SkillIdlesCard for a card that contains all the idles for a specific skill

--- client/src/components/IdleCard/IdleStartStopButton.svelte
+++ client/src/components/IdleCard/IdleStartStopButton.svelte
@@ -4,12 +4,12 @@
 
 {#if cooldown}
     {#if active}
-        <button class="text-red-600">Stop</button>
+        <button class=""><i class="fa-solid fa-stop text-gray-400"></i></button>
     {:else}
-        <button class="text-red-600">Stop</button>
+        <button class=""><i class="fa-solid fa-play text-gray-400"></i></button>
     {/if}
 {:else if active}
-    <button onclick={onStop}>Stop</button>
+    <button class="" onclick={onStop}><i class="fa-solid fa-stop"></i></button>
 {:else}
-    <button onclick={onStart}>Start</button>
+    <button class="" onclick={onStart}><i class="fa-solid fa-play"></i></button>
 {/if}
---
+++ client/src/components/IdleCard/SkillIdlesCard.svelte
@@ -0,0 +1,76 @@
+<script>
+    import { flip } from "svelte/animate";
+    import IdleCard from "./IdleCard.svelte";
+    import { fly } from "svelte/transition";
+    import { cubicOut } from "svelte/easing";
+
+    const { idles } = $props();
+    const skillName = $derived(idles[0].skill);
+    let showList = $state(false);
+
+    let selected = $state(idles[0].idle_id);
+
+    let sortedIds = $state(
+        idles
+            .map((i) => i.idle_id)
+            .sort((a, b) => {
+                if (a === selected) return -1;
+                if (b === selected) return 1;
+                return 0;
+            })
+    );
+    const idlesById = $derived(new Map(idles.map((i) => [i.idle_id, i])));
+
+    function toggleShowList() {
+        showList = !showList;
+    }
+
+    function selectIdle(idleId) {
+        selected = idleId;
+        sortedIds = sortedIds.sort((a, b) => {
+            if (a === selected) return -1;
+            if (b === selected) return 1;
+            return 0;
+        });
+    }
+</script>
+
+<div
+    class="relative max-w-72 my-5 bg-card text-card-foreground border-border border-1 rounded-2xl min-w-72"
+>
+    <div class="flex gap-x-1 justify-around text-center my-0 py-0">
+        <div class=""></div>
+        <div class="my-0 py-0">
+            <h1>{skillName}</h1>
+        </div>
+        <div class="">
+            <button onclick={toggleShowList}>Select</button>
+        </div>
+    </div>
+    {#if showList}
+        <div
+            class="absolute flex flex-col bg-popover rounded-b-2xl z-1 border-border border-x-1 border-b-1 w-full"
+        >
+            {#each sortedIds as id (id)}
+                <button
+                    onclick={() => selectIdle(id)}
+                    animate:flip={{
+                        duration: 400,
+                    }}
+                    disabled={!idlesById.get(id).unlocked}
+                >
+                    <IdleCard
+                        idle={idlesById.get(id)}
+                        selected={selected === id}
+                        controls={false}
+                    />
+                </button>
+            {/each}
+        </div>
+    {/if}
+    {#if selected && idlesById.has(selected)}
+        <div>
+            <IdleCard idle={idlesById.get(selected)} />
+        </div>
+    {/if}
+</div>
